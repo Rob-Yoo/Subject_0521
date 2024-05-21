9874ab5dbd8a886bc423ad3b947d904ce0e1ade4
Rob-Yoo <jin12243@naver.com>
2024-05-21 16:29:49 +0900

    BMICalculator 프로젝트 세팅 완료

---
+++ BMICalculator/BMICalculator/BMICalculatorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Jinyoung Yoo on 5/21/24.
+//
+
+import UIKit
+
+class BMICalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
